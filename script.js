dc0137e945271e1c546beb0a5e8bd62e29782877
Mariana Savoldi <marianasavodi123@gmail.com>
2021-03-09 21:57:54 -0300

    Requisito 2 completo

---
+++ script.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+  let divUm = document.querySelector("#pixel-board")
+
+  function addPixelTable() {
+    for (let index = 0; index <= 5; index += 1){
+      let linhaPx = document.createElement('div');
+      for (let colunaPx = 1 columPixel <= 5; colunaPx += 1) {
+        let pixel = document.createElement('div');
+        pixel.className = 'pixel';
+        linePixel.appendChild(pixel);
+      }
+      divUm.appendChild(linhaPx);
+    }
+  }
+  addPixelTable();
+}
